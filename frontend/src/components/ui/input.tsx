052c9304d541a47e79ff42332171bc22e1fc7c1a
gouthamk29 <goutham.ckm@hotmail.com>
2026-05-13 16:50:24 +0530

    Added workspace

---
+++ frontend/src/components/ui/input.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "border-input file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 disabled:bg-input/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 h-8 w-full min-w-0 rounded-lg border bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-3 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 md:text-sm",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+interface InputWithIconProps extends React.ComponentProps<"input"> {
+  icon?: React.ReactNode;
+}
+
+function InputWithIcon({
+  className,
+  type,
+  icon,
+  value,
+  placeholder,
+  ...props
+}: InputWithIconProps) {
+  const hasValue = String(value ?? "").length > 0;
+
+  return (
+    <div className="relative w-full">
+      {!hasValue && icon && (
+        <div className="text-muted-foreground pointer-events-none absolute top-1/2 left-3 flex -translate-y-1/2 items-center gap-2">
+          {icon}
+
+          {placeholder && <span className="text-sm">{placeholder}</span>}
+        </div>
+      )}
+
+      <input
+        type={type}
+        value={value}
+        data-slot="input"
+        className={cn(
+          "border-input focus-visible:border-ring focus-visible:ring-ring/50 disabled:bg-input/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 h-8 w-full min-w-0 rounded-lg border bg-transparent px-3 py-1 text-base transition-colors outline-none focus-visible:ring-3 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 md:text-sm",
+          className,
+        )}
+        {...props}
+      />
+    </div>
+  );
+}
+export { Input, InputWithIcon };
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,17 +1,51 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { HamburgerIcon, LogOut, Menu, Plus, Search } from "lucide-react";
+import { Input, InputWithIcon } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+import { Separator } from "@/components/ui/separator";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 interface User {
   _id: string;
   email: string;
 }
 
+interface Workspace {
+  name: string;
+  description: string;
+}
+
 const Dashboard = () => {
   const { token, logout } = useAuth();
-
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [data, setUserData] = useState<User | null>(null);
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const [workspace, SetWorkspace] = useState<Workspace[]>([]);
 
   useEffect(() => {
     async function getUserData() {
@@ -49,8 +83,75 @@ const Dashboard = () => {
   console.log(data);
 
   return (
-    <div>
-      <h1>DASHBOARD</h1>
+    <div className="flex h-full min-h-dvh flex-col">
+      <nav className="my-4 flex justify-between px-4">
+        <div className="mx-2 flex-1">
+          <InputWithIcon
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            icon={<Search size={20} />}
+            placeholder="Search Workspace"
+          />
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">UserProfile</Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuItem
+                onClick={() => {
+                  logout();
+                  navigate(0);
+                }}
+              >
+                <LogOut />
+                LogOut
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+      <Separator className="my-2" />
+      <main className="mx-4">
+        <div>
+          <h1 className="text-4xl">My WorkSpace</h1>
+        </div>
+        <div className="my-2 flex min-h-20 min-w-20 rounded-md border">
+          {workspace.length < 1 && (
+            <div className="flex w-full items-center justify-center">
+              <CreateWorkspaceDialog setWorkSpace={SetWorkspace} />
+            </div>
+          )}
+
+          {workspace.length > 0 && (
+            <div className="w-full">
+              <div className="flex flex-wrap gap-2 p-4">
+                {workspace.map((w) => {
+                  return (
+                    <div className="flex min-h-20 w-full max-w-80 min-w-60 flex-col rounded-sm border px-2 py-3">
+                      <div>
+                        <Label className="font-extrabold">{w.name}</Label>
+                      </div>
+                      <div className="mx-1 text-right font-light text-gray-600">
+                        {w.description}
+                      </div>
+                      <div className="flex justify-end">
+                        <Button>View</Button>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="mx-2 my-4 flex justify-end">
+                <CreateWorkspaceDialog setWorkSpace={SetWorkspace} />
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
 
       {data && (
         <div>
@@ -63,3 +164,83 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+interface CreateWorkspaceDialogProps {
+  setWorkSpace: React.Dispatch<React.SetStateAction<Workspace[]>>;
+}
+
+export function CreateWorkspaceDialog({
+  setWorkSpace,
+}: CreateWorkspaceDialogProps) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [open, setOpen] = useState(false);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const workspace = {
+      name,
+      description,
+    };
+
+    console.log(workspace);
+
+    setWorkSpace((w) => [...w, workspace]);
+
+    setName("");
+    setDescription("");
+    setOpen(!open);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Create a new Workspace
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent showCloseButton={false}>
+        <DialogHeader>
+          <DialogTitle>Add New Workspace</DialogTitle>
+        </DialogHeader>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="workspace-name">Workspace Name</Label>
+
+            <Input
+              id="workspace-name"
+              placeholder="My Workspace"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="workspace-description">Description</Label>
+
+            <Textarea
+              id="workspace-description"
+              placeholder="Describe your workspace..."
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="outline">
+                Cancel
+              </Button>
+            </DialogClose>
+
+            <Button type="submit">Create Workspace</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
